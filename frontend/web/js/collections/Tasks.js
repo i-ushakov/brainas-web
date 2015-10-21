4e3dbd19c6fd1015a14003dd37cfb38599ef6420
Kit Ushakov <kitushakoff@gmail.com>
2015-10-21 10:13:49 +0300

    Integrate Backbone, first task from BD (dirty code)

---
+++ frontend/web/js/collections/Tasks.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Kit Ushakov on 10/20/2015.
+ */
+
+var Tasks = Backbone.Collection.extend({
+    model: Task,
+
+    url: '/frontend/web/index.php?r=task/get',
+
+    /*parse: function(response) {
+     debugger;
+     //new Task(response[1])
+     return response.results;
+     }*/
+
+    /*parse: function(response) {
+     debugger;
+     return _(response.rows).map(function(row) { return row.value ;});
+     }*/
+
+    3123213
+});
