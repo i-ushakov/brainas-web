123cd0a1ac78e8fa4ff02b5ca846f95ad6cdfbbc
Kit Ushakov <kitushakoff@gmail.com>
2016-01-06 15:29:22 +0300

    Alpha 0.05 Google Identity Toolkit

---
+++ frontend/views/site/login.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-login">
+    <div id="gitkitWidgetDiv"></div>
+</div>
