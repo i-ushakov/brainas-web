c7617a29571c44d0c1644738ce21fd1850cd7ce4
Kit Ushakov <kitushakoff@gmail.com>
2016-01-22 16:11:31 +0300

    Alpha 0.05 GIT  Integrate in Android app

--- backend/controllers/ConnectionController.php
+++ backend/controllers/ConnectionController.php
@@ -59,7 +59,7 @@ return $response->send();
         $xmlWithTasks = "";
         $xmlWithTasks .= '<?xml version="1.0" encoding="UTF-8"?>';
         // Fox TEST
-        $xmlWithTasks .= '<accessToken>' . $_POST['accessToken'] . '</accessToken>>';
+        $xmlWithTasks .= '<accessToken>' . (isset($_POST['accessToken']) ? $_POST['accessToken'] : "") . '</accessToken>>';
         // For TEST (END)
         $xmlWithTasks .= '<tasks>';
 
