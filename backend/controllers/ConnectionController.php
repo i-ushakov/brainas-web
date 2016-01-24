40e229e02cd673cec6b8f35ba5d7fa71e6f29c70
Kit Ushakov <kitushakoff@gmail.com>
2016-01-24 19:09:37 +0300

    Alpha 0.05 GIT  Integrate in Android app

--- backend/controllers/ConnectionController.php
+++ backend/controllers/ConnectionController.php
@@ -6,16 +6,23 @@
  * Time: 1:01 PM
  */
 
+
+
 namespace backend\controllers;
 
+use common\models\User;
 use Yii;
 use common\infrastructure\ChangedTask;
 use common\models\Task;
 use yii\helpers\Json;
 use yii\web\Controller;
+use yii\web\HttpException;
 
 class ConnectionController extends Controller {
 
+    public static $jsonGoogleClientConfig = "/var/www/brainas.net/backend/config/client_secret_821865067743-3jra19eq308up54c436e1g6fpmvef1g1.apps.googleusercontent.com.json";
+
+    private $userId;
 
     public function beforeAction($action) {
         $this->enableCsrfValidation = false;
@@ -23,21 +30,11 @@ class ConnectionController extends Controller {
         return parent::beforeAction($action);
     }
 
-    public function actionGetTasks()
-    {
-        /*
-         * $response = Yii::$app->getResponse();
-$response->headers->set('Content-Type', 'image/jpeg');
-$response->format = Response::FORMAT_RAW;
-if ( !is_resource($response->stream = fopen($imgFullPath, 'r')) ) {
-   throw new \yii\web\ServerErrorHttpException('file access failed: permission deny');
-}
-return $response->send();
-         */
-
-        //$response = Yii::$app->getResponse();
-        //$response->headers->set('Content-Type', 'image/jpeg');
-
+    public function actionGetTasks() {
+        $accessToken = $this->getTokenFronmPost();
+        $client = $this->prepareGoogleClient($accessToken);
+        $userEmail = $this->verifyIdToken($client, $accessToken);
+        $this->userId = $this->getUserIdByEmail($userEmail);
         $changedTasks = $this->getChangedTasks();
 
         $created = array();
@@ -58,9 +55,6 @@ return $response->send();
 
         $xmlWithTasks = "";
         $xmlWithTasks .= '<?xml version="1.0" encoding="UTF-8"?>';
-        // Fox TEST
-        $xmlWithTasks .= '<accessToken>' . (isset($_POST['accessToken']) ? $_POST['accessToken'] : "") . '</accessToken>';
-        // For TEST (END)
         $xmlWithTasks .= '<tasks>';
 
         // New (created) Tasks
@@ -149,16 +143,67 @@ return $response->send();
     }
 
     private function getChangedTasks() {
-        $userId = $this->getUserId();
         $changedTasks = ChangedTask::find()
-            ->where(['user_id' => $userId])
+            ->where(['user_id' => $this->userId])
             ->orderBy('datetime')
             ->all();
         return $changedTasks;
     }
 
-    private function getUserId() {
-        $userId = 1;
-        return $userId;
+    private function getTokenFronmPost() {
+        $post = Yii::$app->request->post();
+        $post['accessToken'] = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjFmYmU4OTA5M2JkZTk1NTc1YTMwMDY1OTlmMWExOWQyMzViMzgzYjcifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhdWQiOiI4MjE4NjUwNjc3NDMtM2pyYTE5ZXEzMDh1cDU0YzQzNmUxZzZmcG12ZWYxZzEuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTc0MzAxNDA5ODYwNTg4NjgzOTgiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXpwIjoiODIxODY1MDY3NzQzLWo4bDJwZjJyaXBycGFwNzVsY2RxYmZ1bTJiYTY4cXR1LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiZW1haWwiOiJraXR1c2hha29mZkBnbWFpbC5jb20iLCJpYXQiOjE0NTM2Mjg1MDMsImV4cCI6MTQ1MzYzMjEwMywicGljdHVyZSI6Imh0dHBzOi8vbGg0Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tZzNHWDRXLVFxSkkvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQkEvQ25rcnhOMVMydzAvczk2LWMvcGhvdG8uanBnIiwibG9jYWxlIjoiZW4iLCJuYW1lIjoiS2l0IFVzaGFrb3YiLCJnaXZlbl9uYW1lIjoiS2l0IiwiZmFtaWx5X25hbWUiOiJVc2hha292In0.ZKg_V1TE8gYgp1o-qig743kJFwRWKaKZnLvCos-4VlSlmvVK8ORFP2MVp_KtkDJ_Q946thLEnrknEQUGjUlwPd6vq6lHUPm-a5KufbUy6hfAXArZ2XdrpnorpWAAG7kRoVhTbFCPl-qRHaKO6y-5BUxfYVMjZ2dYQYRO0VmxfujNs9YYe1apLFKIZU3ulS9ZQ3Gi6ZxrPvnSvu5Kj0bneY1hQg-pgkt0FqBk67TUbtsaiCiZtC4TW1Qs6xtjb0X6zV1W6owB8mwGhJ04W8wr6foM7YGTk8G5UKehb_q2hsfoQUb6JIFYXSBGnV8tz-vLDCgAyi_I4RJVIsuhYTG4aQ";
+        if(isset($post['accessToken'])) {
+            $accessToken = $post['accessToken'];
+        } else {
+            throw new HttpException(401 ,'Token wasn\'t sent');
+        }
+        return $accessToken;
     }
+
+    private function prepareGoogleClient() {
+        $client = new \Google_Client();
+        $client->setAuthConfigFile(self::$jsonGoogleClientConfig);
+        $client->setAccessType('online'); // default: offline
+        $client->setApplicationName('Brain Assistent');
+        //$client->setClientId(Yii::$app->params['OAuth2ClientIdFroWebApp']);
+        //$client->setClientSecret(Yii::$app->params['OAuth2ClientSecretFroWebApp']);
+        //$scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
+        //$client->setRedirectUri($scriptUri);
+        //$client->setDeveloperKey(Yii::$app->params['androidAPIKey']); // API key
+
+        /*
+        https://developers.google.com/api-client-library/php/auth/web-app (Using OAuth 2.0 for Web Server Applications)
+        http://www.sanwebe.com/2012/11/login-with-google-api-php
+        http://enarion.net/programming/php/google-client-api/google-client-api-php/
+        */
+
+        return $client;
+    }
+
+    private function verifyIdToken($client, $accessToken) {
+        if ($accessToken) {
+            try {
+                $userInfo = $client->verifyIdToken($accessToken);
+            } catch (\Firebase\JWT\ExpiredException  $e) {
+                throw new HttpException(401 ,'Expired token');
+            }
+            if (isset($userInfo) && !is_null($userInfo) && isset($userInfo['email'])) {
+                return $userInfo['email'];
+            } else {
+                throw new HttpException(401 ,'Token is not valid');
+            }
+        }
+    }
+
+    private function getUserIdByEmail($userEmail) {
+        $user = User::findOne(['username' => $userEmail]);
+        if (!empty($user)) {
+            return $user->id;
+        } else {
+            // TODO create new user
+            return null;
+        }
+    }
+
 }
