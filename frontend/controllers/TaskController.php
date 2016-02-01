7d076422fb22cdb0989ab34e6558265ebc3e3f20
Kit Ushakov <kitushakoff@gmail.com>
2016-02-01 17:38:26 +0300

    Test

--- frontend/controllers/TaskController.php
+++ frontend/controllers/TaskController.php
@@ -199,6 +199,7 @@ class TaskController extends Controller {
     }
 
     private function checkThatUserIsNotAGuest() {
+        echo "1111"; exit();
         if (Yii::$app->user->isGuest) {
             $this->result['status'] = "FAILED";
             $this->result['type'] = "must_be_signed_in";
