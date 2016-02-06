b3831b362061ed7cc0baf889962d2b6912e07d41
Kit Ushakov <kitushakoff@gmail.com>
2016-02-06 14:52:56 +0300

    Test

--- backend/controllers/ConnectionController.php
+++ backend/controllers/ConnectionController.php
@@ -216,7 +216,7 @@ class ConnectionController extends Controller {
     private function processAllChangesFromDevice() {
         $allChangesInJSON = file_get_contents($_FILES['all_changes_json']['tmp_name']);
         $allChangesInArray = Json::decode($allChangesInJSON, true);
-        $taskChanges = $allChangesInArray['tasks'];
-        var_dump($taskChanges);
+        //$taskChanges = $allChangesInArray['tasks'];
+        var_dump($allChangesInArray);
     }
 }
