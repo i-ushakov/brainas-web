05fe4bc6f123cad63759b880989d834e048418aa
Kit Ushakov <kitushakoff@gmail.com>
2016-02-09 17:32:51 +0300

    v. 0.06 Creatibg task from device and two direction syncronization

--- backend/controllers/ConnectionController.php
+++ backend/controllers/ConnectionController.php
@@ -263,8 +263,8 @@ class ConnectionController extends Controller {
         $task->message = (String)$newTaskFromDevice->message;
         $task->user = $this->userId;
         $task->save();
-        echo "!!!!!";var_dump((String)$newTaskFromDevice->change[0]->changeDatetime);
-        //$task->loggingChangesForSync("CREATED", (String)$newTaskFromDevice->change[0]->changeDatetime);
+        $changeDatetime = (String)$newTaskFromDevice->change[0]->changeDatetime;
+        $task->loggingChangesForSync("CREATED", $changeDatetime);
         return $task->id;
     }
 }
