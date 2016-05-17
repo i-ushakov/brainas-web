c9a471c9e74f83dc02c157a7b4a9059fa0fe98ae
Kit Ushakov <kitushakoff@gmail.com>
2016-05-17 13:21:43 +0300

    v. 0.08 add unit testing

---
+++ tests/codeception/backend/unit/fixtures/data/events.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'event1' => [
+        'id' => 1,
+        'conditions_id' => 1,
+        'type' => 1,
+        'params' => '{"lat":52.482780222078,"lng":80.859375,"radius":100,"address":"Unnamed Road,  ????????? ????,  ??????","placeId":"ChIJKa-aStiI5UIRYqrJSBALe3M"}'
+    ]
+];
