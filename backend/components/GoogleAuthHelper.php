a9b9e2807ca78ff4f8ac1b7f80db3ab60d908a4c
Kit Ushakov <kitushakoff@gmail.com>
2016-06-10 16:26:47 +0300

    Separate requests for auth and sync (v.0.08)

---
+++ backend/components/GoogleAuthHelper.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kit Ushakov
+ * Date: 6/7/2016
+ * Time: 12:38 PM
+ */
+
+namespace backend\components;
+
+use Google_Client;
+use Yii;
+use common\models\User;
+
+
+class GoogleAuthHelper {
+    public static $jsonGoogleClientConfig = "/var/www/brainas.net/backend/config/client_secret_925705811320-cenbqg1fe5jb804116oefl78sbishnga.apps.googleusercontent.com.json";
+
+    static public  function getGoogleClient() {
+        $client = new Google_Client();
+        $client->setAuthConfigFile(self::$jsonGoogleClientConfig);
+        //$client->setRedirectUri("http://brainas.net/backend/web/connection/");
+        $client->setRedirectUri("http://brainas.net/site/login");
+        $client->setScopes("https://www.googleapis.com/auth/plus.login");
+        $client->setAccessType('online'); //offline
+        $client->setApprovalPrompt('force');
+
+        return $client;
+    }
+
+    static public  function getAccessTokenByCode($code) {
+        $client = self::getGoogleClient();
+        if ($code != null) {
+            $client->authenticate($code);
+            $token = $client->getAccessToken();
+        } else {
+            throw new HttpException(471 ,'Code wasn\'t sent');
+        }
+
+        // Get user's date (email) and user id in our system
+        $data = $client->verifyIdToken();
+        $userEmail = $data['email'];
+        $userId = self::getUserIdByEmail($userEmail);
+        $token = self::actualizeRefreshToken($token, $userId);
+
+        return $token;
+    }
+
+    static public function verifyUserAccess($token) {
+        $authInfo = array();
+        // Create Google client and get accessToken
+        $client = GoogleAuthHelper::getGoogleClient();
+        $client->setAccessToken($token);
+        if ($client->isAccessTokenExpired()) {
+            if (isset($token['refresh_token'])) {
+                $client->refreshToken($token['refresh_token']);
+                $token = $client->getAccessToken();
+            } else {
+                throw new InvalidArgumentException();
+            }
+        }
+
+        // Get user's date (email) and user id in our system
+        $data = $client->verifyIdToken();
+        $userEmail = $data['email'];
+        $userId = self::getUserIdByEmail($userEmail);
+
+        $token = self::actualizeRefreshToken($token, $userId);
+        $authInfo['userId'] = $userId;
+        $authInfo['token'] = $token;
+        return $authInfo;
+    }
+
+    /*
+     *  Save refresh token if exist or retrieve from database and send to client
+     */
+    static private function actualizeRefreshToken($token, $userId) {
+        if (isset($token['refresh_token'])) {
+            self::saveRefreshToken($token['refresh_token'], $userId);
+        } else {
+            $refreshToken = self::getRefreshToken($userId);
+            if ($refreshToken != null) {
+                $token['refresh_token'] = $refreshToken;
+            }
+        }
+        return $token;
+    }
+
+    static private function getUserIdByEmail($userEmail) {
+        $user = User::findOne(['username' => $userEmail]);
+        if (!empty($user)) {
+            return $user->id;
+        } else {
+            $user = new User();
+            $user->username = $userEmail;
+            $user->email = $userEmail;
+            $user->save();
+            return $user->id;
+        }
+    }
+
+    static private function saveRefreshToken($refreshToken, $userId) {
+        $params = [
+            ':user_id' => $userId,
+            ':refresh_token' => $refreshToken,
+        ];
+
+        Yii::$app->db->createCommand('
+                INSERT INTO refresh_tokens (user_id, refresh_token)
+                VALUES(:user_id, :refresh_token) ON DUPLICATE KEY UPDATE
+                user_id=:user_id, refresh_token=:refresh_token
+            ')
+            ->bindValues($params)
+            ->execute();
+        return;
+    }
+
+    static private function getRefreshToken($userId) {
+        $refreshToken = null;
+        $params = [':user_id' => $userId];
+        $r = Yii::$app->db->createCommand('SELECT * FROM refresh_tokens WHERE user_id=:user_id')
+            ->bindValues($params)
+            ->queryOne();
+
+        if (isset($r['refresh_token'])) {
+            $refreshToken = $r['refresh_token'];
+        }
+
+        return $refreshToken;
+    }
+}
--- backend/controllers/ConnectionController.php
+++ backend/controllers/ConnectionController.php
@@ -12,16 +12,16 @@ namespace backend\controllers;
 
 use Google_Client;
 use Yii;
-use common\models\User;
-use common\infrastructure\ChangeOfTask;
-use common\models\Task;
 use backend\components\TaskSyncHelper;
+use backend\components\GoogleAuthHelper;
 
 
 use yii\web\Controller;
 use yii\web\HttpException;
 
 class ConnectionController extends Controller {
+
+    const STATUS_INVALID_TOKEN = "INVALID_TOKEN";
 
     public static $jsonGoogleClientConfig = "/var/www/brainas.net/backend/config/client_secret_925705811320-cenbqg1fe5jb804116oefl78sbishnga.apps.googleusercontent.com.json";
 
@@ -35,84 +35,37 @@ class ConnectionController extends Controller {
         return parent::beforeAction($action);
     }
 
+    public function actionFetchAccessToken() {
+        $code = $this->getCodeFromPost();
+        $token = GoogleAuthHelper::getAccessTokenByCode($code);
+        echo json_encode($token);
+    }
+
     public function actionGetTasks() {
         // get user id and access token
-        $token = $this->verifyUserAccess();
-
+        Yii::warning("actionGetTasks");
+        $token = $this->getTokenFromPost();
+        try {
+            $authInfo = GoogleAuthHelper::verifyUserAccess($token);
+        } catch (\InvalidArgumentException $e) {
+            Yii::warning("catch InvalidArgumentException");
+            echo self::STATUS_INVALID_TOKEN;
+            exit();
+        }
+        $token = $authInfo['token'];
+        $userId =  $authInfo['userId'];
         // retrive xml-document with device changes from file
         $deviceChanges = $this->loadDeviceChangesFromXML();
-        $taskSyncHelper = new TaskSyncHelper($deviceChanges, $this->userId, $token);
+        $taskSyncHelper = new TaskSyncHelper($deviceChanges, $userId, $token);
         $xmlResponse = $taskSyncHelper->doSynchronizationOfTasks();
 
         echo $xmlResponse;
     }
 
 
-    private function verifyUserAccess() {
-        // Create Google client and get accessToken
-        $client = $this->getGoogleClient();
-        $token = $this->getAccessToken($client);
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshToken($token['refresh_token']);
-            $token = $client->getAccessToken();
-        }
-
-        // Get user's date (email) and user id in our system
-        $data = $client->verifyIdToken();
-        $userEmail = $data['email'];
-        $this->userId = $this->getUserIdByEmail($userEmail);
-
-        // save refresh token if exist or retrieve from database and send to client
-        if (isset($token['refresh_token'])) {
-            $this->saveRefreshToken($token['refresh_token']);
-        } else {
-            $refreshToken = $this->getRefreshToken();
-            if ($refreshToken != null) {
-                $token['refresh_token'] = $refreshToken;
-            }
-        }
-        $this->token = $token;
-        return $token;
-    }
-
-    private static function getGoogleClient() {
-        $client = new Google_Client();
-        $client->setAuthConfigFile(self::$jsonGoogleClientConfig);
-        //$client->setRedirectUri("http://brainas.net/backend/web/connection/");
-        $client->setRedirectUri("http://brainas.net/site/login");
-        $client->setScopes("https://www.googleapis.com/auth/plus.login");
-        $client->setAccessType('online'); //offline
-        $client->setApprovalPrompt('force');
-
-        return $client;
-    }
-
-    private function getAccessToken($client) {
-        $code = $this->getCodeFromPost();
-        if ($code != null) {
-            $client->authenticate($code);
-            $token = $client->getAccessToken();
-        } else {
-            $token = json_decode($this->getTokenFromPost(), true);
-            $client->setAccessToken($token);
-        }
-        return $token;
-    }
-
     private function loadDeviceChangesFromXML() {
         $deviceChangesInXML = simplexml_load_file($_FILES['all_changes_xml']['tmp_name']);
         return $deviceChangesInXML;
-    }
-
-    private function getTokenFromPost() {
-        $post = Yii::$app->request->post();
-        //$post['accessToken'] = "";
-        if(isset($post['accessToken'])) {
-            $accessToken = $post['accessToken'];
-        } else {
-            throw new HttpException(470 ,'Token wasn\'t sent');
-        }
-        return $accessToken;
     }
 
     private function getCodeFromPost() {
@@ -125,46 +78,14 @@ class ConnectionController extends Controller {
         return null;
     }
 
-    private function getUserIdByEmail($userEmail) {
-        $user = User::findOne(['username' => $userEmail]);
-        if (!empty($user)) {
-            return $user->id;
+    private function getTokenFromPost() {
+        $post = Yii::$app->request->post();
+        //$post['accessToken'] = "";
+        if(isset($post['accessToken'])) {
+            $accessToken = $post['accessToken'];
         } else {
-            $user = new User();
-            $user->username = $userEmail;
-            $user->email = $userEmail;
-            $user->save();
-            return $user->id;
+            throw new HttpException(470 ,'Token wasn\'t sent');
         }
-    }
-
-    private function getRefreshToken() {
-        $refreshToken = null;
-        $params = [':user_id' => $this->userId];
-        $r = Yii::$app->db->createCommand('SELECT * FROM refresh_tokens WHERE user_id=:user_id')
-            ->bindValues($params)
-            ->queryOne();
-
-        if (isset($r['refresh_token'])) {
-            $refreshToken = $r['refresh_token'];
-        }
-
-        return $refreshToken;
-    }
-
-    private function saveRefreshToken($refreshToken) {
-        $params = [
-            ':user_id' => $this->userId,
-            ':refresh_token' => $refreshToken,
-        ];
-
-        Yii::$app->db->createCommand('
-                INSERT INTO refresh_tokens (user_id, refresh_token)
-                VALUES(:user_id, :refresh_token) ON DUPLICATE KEY UPDATE
-                user_id=:user_id, refresh_token=:refresh_token
-            ')
-            ->bindValues($params)
-            ->execute();
-        return;
+        return json_decode($accessToken, true);
     }
 }
