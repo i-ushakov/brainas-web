63d6f7a48bbead5fbbdd3eab21fa0a3a7a37d442
Kit Ushakov <kit.ushakoff@gmail.com>
2016-10-25 12:29:53 +0300

    test  ( v 0.1)

--- common/models/Task.php
+++ common/models/Task.php
@@ -86,6 +86,7 @@ class Task extends ActiveRecord {
     }
 
     public function updateStatus() {
+        Yii::warning("before ==".$this->status);
         $conditions = $this->conditions;
         if ($this->status == 'TODO' && !empty($conditions)) {
             $this->status = "WAITING";
@@ -96,5 +97,6 @@ class Task extends ActiveRecord {
         if(($this->status == 'WAITING' || $this->status == 'ACTIVE') && empty($conditions)) {
             $this->status = "DISABLED";
         }
+        Yii::warning("after ==".$this->status);
     }
 }
