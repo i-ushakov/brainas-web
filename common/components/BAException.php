20e747903c7a99c65a21538f1e8bd585bb637ffb
Kit Ushakov <kit.ushakoff@gmail.com>
2016-12-01 08:38:17 +0300

    add custon BAException

---
+++ common/components/BAException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kit
+ * Date: 11/30/2016
+ * Time: 10:24 AM
+ */
+
+namespace common\components;
+
+class BAException extends \Exception {
+    const WRONG_NAME_OF_EVENT_TYPE_ERRORCODE = 1101;
+}
