1493d6046d8aaa68e68776f79806ed23eca9a36c
Kit Ushakov <kit.ushakoff@gmail.com>
2016-12-30 15:09:25 +0300

    Some chgenges

--- common/components/BAException.php
+++ common/components/BAException.php
@@ -10,4 +10,9 @@ namespace common\components;
 
 class BAException extends \Exception {
     const WRONG_NAME_OF_EVENT_TYPE_ERRORCODE = 1101;
+    const INCORRECT_SIMPLE_XML_OBJEST_ERRORCODE = 1102;
+
+    public function __construct($message, $code, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
 }
