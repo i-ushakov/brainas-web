04a8528a15a924372920b3c4a73d7f93ddaf215d
Kit Ushakov <kit.ushakoff@gmail.com>
2017-03-05 11:31:00 +0300

    Addd codes

--- common/components/BAException.php
+++ common/components/BAException.php
@@ -11,8 +11,10 @@ namespace common\components;
 class BAException extends \Exception {
     const WRONG_NAME_OF_EVENT_TYPE_ERRORCODE = 1101;
     const INCORRECT_SIMPLE_XML_OBJEST_ERRORCODE = 1102;
+    const EMPTY_PARAM_EXCODE = 1103;
+    const INVALID_PARAM_EXCODE = 1104;
 
-    public function __construct($message, $code, Exception $previous = null) {
+    public function __construct($message, $code, \Exception $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 }
