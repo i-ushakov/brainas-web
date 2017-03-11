1b54892127aa892e9f3f412a667178f81d48f1a4
Kit Ushakov <kit.ushakoff@gmail.com>
2017-03-11 14:00:08 +0300

    1102 error code is vacantional

--- common/components/BAException.php
+++ common/components/BAException.php
@@ -10,9 +10,10 @@ namespace common\components;
 
 class BAException extends \Exception {
     const WRONG_NAME_OF_EVENT_TYPE_ERRORCODE = 1101;
-    const INCORRECT_SIMPLE_XML_OBJEST_ERRORCODE = 1102;
+// 1102
     const EMPTY_PARAM_EXCODE = 1103;
     const INVALID_PARAM_EXCODE = 1104;
+    const WRONG_ROOT_XML_ELEMENT_NAME = 1105;
 
     public function __construct(string $message, int $code, \Exception $previous = null) {
         parent::__construct($message, $code, $previous);
