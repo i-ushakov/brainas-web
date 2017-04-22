4957d8bad41fe235ca496200e5dd675347846ed4
Kit Ushakov <kit.ushakoff@gmail.com>
2017-04-22 13:20:19 +0300

    Add new tests

---
+++ tests/codeception/backend/unit/components/XMLResponseBuilder/XMLResponseBuilder_PrepareSyncObjectsXml_Test.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kit
+ * Date: 3/5/2017
+ * Time: 3:48 PM
+ */
+
+use backend\components\XMLResponseBuilder;
+
+
+use AspectMock\Test as test;
+use Mockery as m;
+
+class XMLResponseBuilder_PrepareSyncObjectsXml_Test extends \Codeception\TestCase\Test
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    protected function tearDown()
+    {
+        test::clean(); // remove all registered test doubles
+        m::close();
+    }
+
+    public function testPrepareSyncObjectsXml()
+    {
+        /* var $xmlResponseBuilder XMLResponseBuilder */
+        $xmlResponseBuilder = new XMLResponseBuilder();
+
+        $synchronizedTasks = [
+            1 => 11,
+            2 => 12
+        ];
+
+        // testing ...
+        $result = $xmlResponseBuilder->prepareSyncObjectsXml($synchronizedTasks);
+
+        // assetions :
+        $synchronizedObjsXml = '<?xml version="1.0" encoding="UTF-8"?>' .
+            '<synchronizedTasks>' .
+                '<synchronizedTask>' .
+                    '<localId>1</localId>' .
+                    '<globalId>11</globalId>' .
+                '</synchronizedTask>' .
+                '<synchronizedTask>' .
+                    '<localId>2</localId>' .
+                    '<globalId>12</globalId>' .
+                '</synchronizedTask>' .
+            '</synchronizedTasks>';
+        $this->tester->assertEquals($synchronizedObjsXml, $result, "Wrong xml with synchronized objects");
+    }
+}
---
+++ tests/codeception/backend/unit/components/XMLResponseBuilder/XMLResponseBuilder_PrepareXmlWithTasksChanges_Test.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kit
+ * Date: 3/5/2017
+ * Time: 3:48 PM
+ */
+
+use backend\components\XMLResponseBuilder;
+use common\nmodels\Task;
+use common\infrastructure\ChangeOfTask;
+
+use AspectMock\Test as test;
+use Mockery as m;
+
+class XMLResponseBuilder_PrepareXmlWithTasksChanges_Test extends \Codeception\TestCase\Test
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    protected function tearDown()
+    {
+        test::clean(); // remove all registered test doubles
+        m::close();
+    }
+
+    public function testPrepareXmlWithTasksChanges()
+    {
+        /* var $xmlResponseBuilder XMLResponseBuilder */
+        $xmlResponseBuilder = new XMLResponseBuilder();
+
+        $changedTasks = [
+            'created' => [
+                11 => [
+                    'action' => ChangeOfTask::STATUS_CREATED,
+                    'datetime' => '2017-04-13 20:00:16',
+                    'object' => new Task([
+                        'id' => 11,
+                        'user' => 1,
+                        'message' => 'Task 11',
+                        'description' => 'No desc',
+                        'status' => 'TODO',
+                        'created' => '2017-04-13 20:00:16',
+                        'last_modify' => '2017-04-13 20:00:16'])
+                ]
+            ],
+            'updated' => [
+                12 => [
+                    'action' => ChangeOfTask::STATUS_UPDATED,
+                    'datetime' => '2017-04-13 22:00:00',
+                    'object' => new Task([
+                        'id' => 12,
+                        'user' => 1,
+                        'message' => 'Task 12',
+                        'description' => 'No desc',
+                        'status' => 'ACTIVE',
+                        'created' => '2017-04-13 20:00:16',
+                        'last_modify' => '2017-04-13 22:00:00'])
+                ]
+            ]
+        ];
+
+        // testing ...
+        $result = $xmlResponseBuilder->prepareXmlWithTasksChanges($changedTasks);
+
+        // assetions :
+        $xmlWithTasksChanges = '<?xml version="1.0" encoding="UTF-8"?>' .
+            '<tasks>' .
+                '<created>' .
+                    '<task globalId="11" time-changes="2017-04-13 20:00:16">' .
+                        '<message>Task 11</message>' .
+                        '<description>No desc</description>' .
+                        '<conditions></conditions>' .
+                        '<status>TODO</status>' .
+                    '</task>' .
+                '</created>' .
+                '<updated>' .
+                    '<task globalId="12" time-changes="2017-04-13 22:00:00">' .
+                        '<message>Task 12</message>' .
+                        '<description>No desc</description>' .
+                        '<conditions></conditions>' .
+                        '<status>ACTIVE</status>' .
+                    '</task>' .
+                '</updated>'  .
+            '</tasks>';
+        $this->tester->assertEquals($xmlWithTasksChanges, $result, "Wrong xml with synchronized objects");
+    }
+}
