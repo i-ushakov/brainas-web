abfc63a47c23f080357db01ef324339a0ecb9f78
Kit Ushakov <kit.ushakoff@gmail.com>
2017-05-23 16:35:41 +0300

    Added comments

--- tests/codeception/frontend/functional/TaskController/Get/GetAllTasksCest.php
+++ tests/codeception/frontend/functional/TaskController/Get/GetAllTasksCest.php
@@ -14,8 +14,13 @@ class GetAllTasksCest
         $DBCleaner->clean();
     }
 
-    public function tryToTest(\FunctionalTester $I)
+    // I think we don't need to test this stuff, it will be overhead (YAGNI)
+    public function getAllTest(\FunctionalTester $I)
     {
+        //$I->sendGET('/task/get', ['statusesFilter' => 'ALL', 'typeOfSort' => 'TIME_ADDED_NEWEST']);
+        //$response = $I->grabResponse();
+        //file_put_contents("test.txt1", $response);
+
         //https://brainas.com/task/get?statusesFilter%5B%5D=ACTIVE&statusesFilter%5B%5D=WAITING&statusesFilter%5B%5D=TODO&statusesFilter%5B%5D=DISABLED&statusesFilter%5B%5D=DONE&statusesFilter%5B%5D=CANCELED&typeOfSort=TIME_ADDED_NEWEST
     }
 
