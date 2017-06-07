3c7d4a820523696daa7b72ac4da2a0a7fdf6026d
Kit Ushakov <kit.ushakoff@gmail.com>
2017-06-07 16:46:23 +0300

    removed an unused dependency

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -23,7 +23,6 @@ class AppAsset extends AssetBundle
     public $js = [
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\web\YiiAsset'
     ];
 }
