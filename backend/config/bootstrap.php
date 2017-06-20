ae16e5d39adace676a71083553c62a49e85a8309
Kit Ushakov <kit.ushakoff@gmail.com>
2017-06-20 16:01:41 +0300

    DIC in action

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -4,24 +4,51 @@ use yii\di\Instance;
 use backend\components\ChangeOfTaskParser;
 use backend\components\ChangeOfTaskHandler;
 use backend\components\XMLResponseBuilder;
+use backend\components\TasksSyncManager;
+use backend\components\Factory\GoogleClientFactory;
 use common\components\TaskXMLConverter;
 use common\components\ConditionXMLConverter;
+use \frontend\components\GoogleDriveHelper;
 
 $container = Yii::$container;
 
 $container->setDefinitions([
+
+    GoogleDriveHelper::class => function()
+    {
+        $client = GoogleClientFactory::create();
+        return new GoogleDriveHelper($client);
+    },
+
     ChangeOfTaskParser::class => [
         ['class' => ChangeOfTaskParser::class],
     ],
+
     TaskXMLConverter::class => [
         ['class' => ConditionXMLConverter::class],
     ],
-    ChangeOfTaskHandler::class => [
-        ['class' => ChangeOfTaskHandler::class],
-        [Instance::of(ChangeOfTaskParser::class), Instance::of(TaskXMLConverter::class)]
-    ],
+
+
     XMLResponseBuilder::class => [
         ['class' => XMLResponseBuilder::class],
         [Instance::of(TaskXMLConverter::class)]
+    ],
+
+    ChangeOfTaskHandler::class => [
+        ['class' => ChangeOfTaskHandler::class],
+        [
+            Instance::of(ChangeOfTaskParser::class),
+            Instance::of(TaskXMLConverter::class),
+            null,
+            Instance::of(GoogleDriveHelper::class),
+        ]
+    ],
+
+    TasksSyncManager::class => [
+        ['class' => TasksSyncManager::class],
+        [
+            Instance::of(ChangeOfTaskHandler::class),
+            Instance::of(XMLResponseBuilder::class),
+        ]
     ]
 ]);
