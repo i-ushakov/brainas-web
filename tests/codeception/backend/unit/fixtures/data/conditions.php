418cd667d93089164cbdacc58b461c0ce75aec6e
Kit Ushakov <kit.ushakoff@gmail.com>
2017-07-21 21:36:03 +0300

    deleting unnecessary files

--- tests/codeception/backend/unit/fixtures/data/conditions.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'condition1' => [
-        'id' => 1,
-        'task_id' => 1
-    ]
-];
--- tests/codeception/backend/unit/fixtures/data/tasks.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'task1' => [
-        'id' => 1,
-        'message' => 'TEST_111',
-        'email' => 'test@mail.ru.vernice@jerde.com'
-    ]
-];
