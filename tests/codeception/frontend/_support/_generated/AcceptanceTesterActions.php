3b576804d8cb18cf849ecb4d59bcb443da3c257c
Kit Ushakov <kit.ushakoff@gmail.com>
2017-07-31 19:38:22 +0300

    FixtureHelper

--- tests/codeception/frontend/_support/_generated/AcceptanceTesterActions.php
+++ tests/codeception/frontend/_support/_generated/AcceptanceTesterActions.php
@@ -6,7 +6,6 @@ namespace _generated;
 // @codingStandardsIgnoreFile
 
 use Codeception\Module\PhpBrowser;
-use tests\codeception\common\_support\FixtureHelper;
 use Helper\Acceptance;
 
 trait AcceptanceTesterActions
