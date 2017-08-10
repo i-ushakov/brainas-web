132a36131876a5bf90e9473e2ce4f2929ac855f5
Kit Ushakov <kit.ushakoff@gmail.com>
2017-08-10 14:51:52 +0300

    added new test

--- tests/codeception/frontend/unit/components/TasksManager/CreateNewTaskTest.php
+++ tests/codeception/frontend/unit/components/TasksManager/CreateNewTaskTest.php
@@ -6,7 +6,11 @@
  * Date: 8/9/2017
  * Time: 12:33 PM
  */
-use \frontend\components\TasksManager;
+use common\models\User;
+use frontend\components\TasksManager;
+use frontend\components\StatusManager;
+use frontend\components\GoogleIdentityHelper;
+use frontend\components\ChangesLogger;
 
 use Mockery as m;
 
@@ -24,6 +28,34 @@ class CreateNewTaskTest extends \Codeception\TestCase\Test
 
     public function test()
     {
-        // TODO
+        /* @var array */
+        $taskDataForSave = [
+            'id'=> null,
+            'conditions' => [[]],
+            'picture_name' => 'picture_name',
+            'picture_file_id' => 'picture_file_id'
+        ];
+
+        /* @var $user */
+        $user = m::mock(User::class . "[]");
+        $testUserId = 1;
+        $user->id = $testUserId;
+
+        /* @var $tasksManagerSpy TasksManager */
+        $tasksManagerSpy = m::mock(TasksManager::class . "[setTaskData, saveTask]",
+            [
+                m::mock(GoogleIdentityHelper::class),
+                m::mock(StatusManager::class),
+                m::mock(ChangesLogger::class)
+            ]);
+        $tasksManagerSpy->shouldReceive('setTaskData')->once();
+        $tasksManagerSpy->shouldReceive('saveTask')->once();
+        $tasksManagerSpy->setUser($user);
+
+        $task = $tasksManagerSpy->createTask($taskDataForSave);
+
+        $this->tester->assertEquals(TasksManager::NEW_MESSAGE_TITLE, $task->message);
+        $this->tester->assertEquals($testUserId, $task->user);
     }
+
 }
